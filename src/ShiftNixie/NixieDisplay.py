80a1cbd6c52fb7353cb00964ad3802530de0b735
enriquesleon <enriquesleon>
2015-10-18 22:52:22 -0700

    started work on the Nixie Interface

--- src/ShiftNixie/NixieDisplay.py
+++ src/ShiftNixie/NixieDisplay.py
@@ -4,21 +4,32 @@
 #with an array of integers that the tube will display. If the value is out of range, the value will
 #default to zero in order to avoid blanking the tubes and damaging them. The left most
 #value in the array will be shifted first.
-import shift595
+#import shift595
 import time
-import RPi.GPIO as GPIO
+import math
+#import RPi.GPIO as GPIO
 PIN_SERIAL = 29
 PIN_ENABLE = 31
 PIN_LATCH  = 33
 PIN_CLK    = 35
 PIN_CLR    = 37
 DELAY      = .010
-def class NixieDisplay:
+LSByte 	   = 0x0F 	
+class NixieDisplay:
 	def __init__(self,dNumber_Displays):
 		self.number_displays = number_displays
-		self.shift = shift595(PIN_SERIAL,PIN_ENABLE,PIN_LATCH,PIN_CLK,PIN_CLR,number_displays//2)
+		self.numberRegisters = int(math.ceil(self.number_displays/2))
+		self.shift = shift595(PIN_SERIAL,PIN_ENABLE,PIN_LATCH,PIN_CLK,PIN_CLR,self.numberRegisters)
 		self.currentValues = shift.register_values
-	def upDate(self,dValuesList):
-		MSB = 0x80
-		LSB = 0x00
+	def updateDisplay(self,dValuesList = [0]):
+		pass
+	def combineToRegister(self,dRegisterValue = [0,0]):
+		mostSigRegByte  = dRegisterValue[0]&LSByte
+		leastSigRegByte = dRegisterValue[1]&LSByte
+		mostSigRegByte  = mostSigRegByte << 4
+		registerValue = mostSigRegByte|leastSigRegByte
+		return registerValue
+
+
+
 	
