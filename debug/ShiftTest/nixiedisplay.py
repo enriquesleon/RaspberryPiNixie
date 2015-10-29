76feae65bbdd3dbf4d097ef87686fd5cb1dec241
enriquesleon <enriquesleon>
2015-10-29 00:31:57 -0700

    added some simple exception handling for the display class

--- debug/ShiftTest/nixiedisplay.py
+++ debug/ShiftTest/nixiedisplay.py
@@ -63,7 +63,11 @@ class NixieDisplay:
 		for tube in self.nixie_tubes:
 			tube.set_value(0x0)
 	def set_display(self,index,value):
-		self.nixie_tubes[index].set_value(int(value))
+		try:
+			self.nixie_tubes[index].set_value(int(value))
+		except ValueError:
+			print "Invalid Type Input for {}".format(value)
+			self.nixie_tubes[index].set_value(0x0)	
 	def get_display_value(self,index):
 		return self.nixie_tubes[index].current_value()
 	def blink(self,times = 0,time = 0 ,delay = 10000):
